62febb0ae8efbf95ab454bbb5bf3a3798e032cc7
Gushley110 <gusley_dominator@hotmail.com>
2020-03-21 23:15:52 -0600

    Added custom styles

--- src/index.js
+++ src/index.js
@@ -21,12 +21,13 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 
-import './assets/scss/mdb-free.scss';
+
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/paper-dashboard.scss?v=1.1.0";
 import "assets/demo/demo.css";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 import 'assets/scss/icheck-material/icheck-material.scss';
+import 'assets/scss/custom_styles.scss'
 
 import AdminLayout from "layouts/Admin.jsx";
 
